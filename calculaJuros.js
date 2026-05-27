5cebc92ccf6517d72a7bb440791957cd8872f423
Mendes44 <marcosmendesm10@gmail.com>
2026-05-27 19:21:10 +1000

    Melhoria do Codigo - Usando Estrutura de Repetição para validação do valor e dos dias vencidos

--- calculaJuros.js
+++ calculaJuros.js
@@ -4,31 +4,27 @@ console.log(":::Aplicação de Juros:::\n");
 
 let valorOriginal = entrada.questionFloat('Informe o valor devido: ');
 
-if (valorOriginal > 0) {
+while (valorOriginal <= 0) {
+    console.log("Valor inválido! Para calcular os juros, o valor deve ser maior que zero.\n");
+    valorOriginal = entrada.questionFloat('Informe o valor devido: ');
+}
 
-    let vencimento = entrada.questionInt('Informe quantos dias se passaram desde o vencimento do boleto: ');
+let vencimento = entrada.questionInt('Informe quantos dias se passaram desde o vencimento do boleto: ');
 
-    if (vencimento > 0) {
+while (vencimento < 0) {
+    console.log("Quantidade de dias inválida! Informe zero ou um número positivo.\n");
+    vencimento = entrada.questionInt('Informe quantos dias se passaram desde o vencimento do boleto: ');
+}
 
-        let valorJuros;
+if (vencimento > 0) {
+    let valorJuros = vencimento > 15 ? 10 : 5;
 
-        if (vencimento > 15) {
-            valorJuros = 10;
-        } else {
-            valorJuros = 5;
-        }
+    let valorFinal = valorOriginal + (valorOriginal * (valorJuros / 100));
 
-        let valorFinal = valorOriginal + (valorOriginal * (valorJuros / 100));
-
-        console.log(`Valor Original da dívida: R$${valorOriginal.toFixed(2)}`);
-        console.log(`Dias atrasados: ${vencimento} dias`);
-        console.log(`Valor do juros aplicado: ${valorJuros}%`);
-        console.log(`Valor Final da dívida: R$${valorFinal.toFixed(2)}`);
-
-    } else {
-        console.log("Você está em dia!");
-    }
-
+    console.log(`Valor Original da dívida: R$${valorOriginal.toFixed(2)}`);
+    console.log(`Dias atrasados: ${vencimento} dias`);
+    console.log(`Valor do juros aplicado: ${valorJuros}%`);
+    console.log(`Valor Final da dívida: R$${valorFinal.toFixed(2)}`);
 } else {
-    console.log("Valor inválido! Para calcular os juros, o valor deve ser maior que zero.");
+    console.log("Você está em dia!");
 }
