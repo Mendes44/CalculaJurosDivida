dae125a19dcfaf46c1d66829a22dd64020ee64c0
Mendes44 <marcosmendesm10@gmail.com>
2026-05-27 19:08:15 +1000

    Melhoria do Codigo - Aumento da Logica para verificar

--- calculaJuros.js
+++ calculaJuros.js
@@ -3,10 +3,32 @@ import entrada from 'readline-sync';
 console.log(":::Aplicação de Juros:::\n");
 
 let valorOriginal = entrada.questionFloat('Informe o valor devido: ');
-let tempo = entrada.questionFloat('Informe quantos dias se passaram desde o vencimento do boleto: ');
 
-let valorFinal = tempo > 15 ? valorOriginal * 1.10 : valorOriginal * 1.05;
+if (valorOriginal > 0) {
 
-console.log(`Valor Original da divida: R$${valorOriginal.toFixed(2)}`);
-console.log(`Dias atrasados: ${tempo} dias`);
-console.log(`Valor Final da divida: R$${valorFinal.toFixed(2)}`);
+    let vencimento = entrada.questionInt('Informe quantos dias se passaram desde o vencimento do boleto: ');
+
+    if (vencimento > 0) {
+
+        let valorJuros;
+
+        if (vencimento > 15) {
+            valorJuros = 10;
+        } else {
+            valorJuros = 5;
+        }
+
+        let valorFinal = valorOriginal + (valorOriginal * (valorJuros / 100));
+
+        console.log(`Valor Original da dívida: R$${valorOriginal.toFixed(2)}`);
+        console.log(`Dias atrasados: ${vencimento} dias`);
+        console.log(`Valor do juros aplicado: ${valorJuros}%`);
+        console.log(`Valor Final da dívida: R$${valorFinal.toFixed(2)}`);
+
+    } else {
+        console.log("Você está em dia!");
+    }
+
+} else {
+    console.log("Valor inválido! Para calcular os juros, o valor deve ser maior que zero.");
+}
