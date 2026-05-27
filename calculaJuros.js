2b0dbbf7f8e61572b1ffc97b28c350ec245451bd
Mendes44 <marcosmendesm10@gmail.com>
2026-05-27 18:48:10 +1000

    Melhoria do codigo - Usando Ternário

--- calculaJuros.js
+++ calculaJuros.js
@@ -5,13 +5,7 @@ console.log(":::Aplicação de Juros:::\n");
 let valorOriginal = entrada.questionFloat('Informe o valor devido: ');
 let tempo = entrada.questionFloat('Informe quantos dias se passaram desde o vencimento do boleto: ');
 
-let valorFinal = 0;
-
-if (tempo > 15) {
-    valorFinal = valorOriginal * 1.10;
-} else {
-    valorFinal = valorOriginal * 1.05;
-}
+let valorFinal = tempo > 15 ? valorOriginal * 1.10 : valorOriginal * 1.05;
 
 console.log(`Valor Original da divida: R$${valorOriginal.toFixed(2)}`);
 console.log(`Dias atrasados: ${tempo} dias`);
